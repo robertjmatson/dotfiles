4eb0aba83fa50a74c333ef6cb18f2ddd9205432d
Robert Matson <88677843+robertjmatson@users.noreply.github.com>
2024-08-01 22:58:27 -0500

    added additional packages for arch

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 distro=$(lsb_release -i -s)
 case $distro in
 	"Arch"*)
-		sudo pacman -S chezmoi alacritty xorg-xrandr neovim git lazygit github-cli pavucontrol discord
+		sudo pacman -S chezmoi alacritty xorg-xrandr neovim git lazygit github-cli pavucontrol discord lua51 timeshift nfs-utils samba
 		chezmoi init --apply robertjmatson
+		timedatectl set-timezone America/Chicago
 		;;
 	*)
 		echo "other"
