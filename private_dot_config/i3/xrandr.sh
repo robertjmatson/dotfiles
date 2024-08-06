739aa37e64ed017044cdfea66e8f3fff0c600492
Robert Matson <88677843+robertjmatson@users.noreply.github.com>
2024-08-05 22:43:52 -0500

    updates to i3wm-gaps bar and i3block

--- private_dot_config/i3/xrandr.sh
+++ private_dot_config/i3/xrandr.sh
@@ -4,3 +4,6 @@ xrandr \
 --output HDMI-0 --mode 1920x1080 --pos 320x0 --rotate normal
 #xrandr --setmonitor Virtual-Left 2560/596x1440/340+1080+1080 DP-4
 #xrandr --setmonitor Virtual-Right 2560/596x1440/340+3640+1080 none
+
+#xrandr \
+#--output DP-0 --mode 1920x1080 --pos -1920x1080 --rotate left
--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 distro=$(lsb_release -i -s)
 case $distro in
 	"Arch"*)
-		sudo pacman -S chezmoi alacritty xorg-xrandr neovim git lazygit github-cli pavucontrol discord lua51 timeshift nfs-utils samba
+		sudo pacman -S chezmoi alacritty xorg-xrandr neovim git lazygit github-cli pavucontrol discord lua51 timeshift nfs-utils samba net-tools keepassxc sysstat
 		chezmoi init --apply robertjmatson
 		timedatectl set-timezone America/Chicago
+		git clone https://github.com/vivien/i3blocks-contrib.git ~/.config/i3blocks/scripts	
 		;;
 	*)
 		echo "other"
