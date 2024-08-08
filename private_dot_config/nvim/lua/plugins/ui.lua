a80618eb5fa60f237ae190a02d3f1cd32421b0bc
Robert Matson <88677843+robertjmatson@users.noreply.github.com>
2024-08-08 16:08:48 -0500

    switched from tokyonight to catppuccin-mocha theming

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -28,7 +28,7 @@ return {
 			},
 		},
 	},
-	{ -- You can easily change to a different colorscheme.
+	--[[	{ -- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
@@ -43,6 +43,16 @@ return {
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+]]
+	--
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
 }
