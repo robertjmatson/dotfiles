98825451d094329cc849d5cd20491907482cb768
Robert Matson <88677843+robertjmatson@users.noreply.github.com>
2025-02-12 23:11:49 -0600

    moving to plasma with i3 instead of pure i3wm

---
+++ readme.md
@@ -0,0 +1,13 @@
+Install Chezmoi
+on Arch add Multilib
+sudo nano /etc/pacman.conf
+uncomment multilib
+
+chezmoi init https://github.com/robertjmatson/dotfiles.git
+Run setup.sh
+
+
+
+
+
+
--- setup.sh
+++ setup.sh
@@ -2,23 +2,26 @@
 distro=$(lsb_release -i -s)
 case $distro in
 	"Arch"*)
-		sudo pacman -S xorg-xrandr pavucontrol timeshift sysstat less nano htop arandr xterm base-devel
-		sudo pacman -S i3-wm i3blocks i3status i3lock dmenu
-		sudo pacman -S nfs-utils samba net-tools keepassxc 
-		sudo pacman -S git lazygit github-cli 
-		sudo pacman -S bluez bluez-utils
-		sudo pacman -S ttf-jetbrains-mono-nerd bash
-		sudo pacman -S chezmoi alacritty neovim lua51 luarocks ranger tmux
-		sudo pacman -S discord firefox steam nextcloud-client
-		sudo systemctl enable bluetooth.service
 		sudo systemctl start bluetooth.service
-		chezmoi init --apply robertjmatson
+		sudo systemctl enable bluetooth.service
+
+		sudo pacman -Syu
+		sudo pacman -S i3
+		#create plasma-i3 service
+		systemctl mask plasma-kwin_x11.service --user
+		systemctl enable plasma-i3 --user
+		sudo pacman -S firefox steam discord nextcloud-client
+		sudo pacman -S alacritty tmux feh nitrogen picom dmenu
+		sudo pacmam -S git lazygit github-cli
+		sudo pacman -S samba keepassxc nfs-utils timeshift
+		sudo pacman -S nfs-utils samba net-tools
+		sudo pacman -S neovim lua51 luarocks ranger 
+		sudo pacman -S chezmoi
+		sudo pacman -S ttf-jetbrains-mono-nerd bash
+
 		mkdir ~/.config/alacritty/themes
 		git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
-		timedatectl set-timezone America/Chicago
 		git clone https://github.com/vivien/i3blocks-contrib.git ~/.config/i3blocks/scripts	
-		tmux new-session -d
-		tmux source ~/.config/tmux/tmux.conf
 		;;
 	*)
 		echo "other"
