33176e2f5bc7127e115ac0e2ad02e92d6235532b
Robert Matson <88677843+robertjmatson@users.noreply.github.com>
2025-02-26 21:16:26 -0600

    switching from customized nvim to lazyvim

--- private_dot_config/i3/xrandr.sh
+++
@@ -1,9 +0,0 @@
-xrandr \
---output DP-4 --mode 5120x1440 --pos 0x1080 --rotate normal --primary \
---output DP-2 --mode 1920x1080 --pos 2580x0 --rotate normal \
---output HDMI-0 --mode 1920x1080 --pos 320x0 --rotate normal
-#xrandr --setmonitor Virtual-Left 2560/596x1440/340+1080+1080 DP-4
-#xrandr --setmonitor Virtual-Right 2560/596x1440/340+3640+1080 none
-
-#xrandr \
-#--output DP-0 --mode 1920x1080 --pos -1920x1080 --rotate left
