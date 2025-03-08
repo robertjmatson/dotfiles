ef1662d67ca557b200029e0d5f7d78cca1d693c6
Robert Matson <88677843+robertjmatson@users.noreply.github.com>
2025-03-07 21:32:35 -0600

    removed keymap

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>fx", vim.cmd.Ex, { desc = "[P]roject [E]xplorer" })
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
